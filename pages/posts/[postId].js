ce6153a764c48d926de8131e3ccb54cf0069f29d
Manikandan <manied116@gmail.com>
2022-11-06 12:07:18 +0530

    added getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -8,13 +8,26 @@ function Post({post}) {
     );
 }
 
+export default Post
+
 export async function getStaticPaths(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/')
+
+    const data  = await response.json()
+    const paths = data.map(post => {                                                                                                                                                                                                                                                                                                                                                             
+        return {
+            params: {
+                postId:`${post.id}`
+            }
+        }
+    })
     return{
-        paths:[
-            { params:{postId:'1'},},
-            { params:{postId:'2'},},
-            { params:{postId:'3'},}
-        ],
+        // paths:[
+        //     { params:{postId:'1'},},
+        //     { params:{postId:'2'},},
+        //     { params:{postId:'3'},}
+        // ],
+        paths,
         fallback:false,
     }
 }
@@ -26,12 +39,9 @@ export async function getStaticProps(context){
         )
 
     const data = await response.json()
-    console.log(data)
     return {
         props:{
             post:data,
         },
     }
 }
-
-export default Post
